5ac79f31725b25257d3dd313995335223f80e333
Solderpunk <solderpunk@sdf.org>
2018-01-23 23:34:52 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='VF-1',
-    version='0.0.2',
+    version='0.0.3',
     description="Command line gopher client. High speed, low drag.",
     author="Solderpunk",
     author_email="solderpunk@sdf.org",
