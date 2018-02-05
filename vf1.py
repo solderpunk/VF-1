3ce03d1ca7b43a414e6ec3b8310720f607cfd6b9
Alex Schroeder <alex@gnu.org>
2018-02-05 10:47:35 +0100

    Handle corner cases for do_save

--- vf1.py
+++ vf1.py
@@ -671,9 +671,13 @@ Use 'ls -r' to list in reverse order."""
         subprocess.call(("cat %s |" % self.tmp_filename) + line, shell=True)
 
     def do_save(self, filename):
-        """Save most recently visited item to file."""
+        """Save most recently visited text item to file."""
         filename = os.path.expanduser(filename)
-        if os.path.exists(filename):
+        if not filename:
+            print("Please provide a filename")
+        elif not self.tmp_filename:
+            print("You need to visit a text item, first")
+        elif os.path.exists(filename):
             print("File already exists!")
         else:
             shutil.copyfile(self.tmp_filename, filename)
